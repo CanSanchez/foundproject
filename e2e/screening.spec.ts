1ec84b43d6d3979799b926f2091ee3737614a928
KayceeNguyen <91505214+KayceeNguyen@users.noreply.github.com>
2023-02-14 01:13:17 -0800

    make screening.spec.ts

--- e2e/screening.spec.ts
+++ e2e/screening.spec.ts
@@ -39,3 +39,50 @@ test.describe('Header area', () => {
     })
 
 });
+
+test.describe('Main area', () => {
+    test('Count number of div in main tag', async ({ page }) => {
+        await page.goto(UrlScreening)
+
+        await expect(page.locator('main > div')).toHaveCount(1);
+    });
+
+    // test('Count number of div in div tag', async ({ page }) => {
+    //     await page.goto(UrlScreening)
+
+    //     await expect(page.locator('main > div > div')).toHaveCount(1);
+    // });
+    
+    test('Header Tag', async({ page }) => {
+        await page.goto(UrlScreening)
+
+        await expect(page.locator('h1')).toContainText('Tell us what happened?');
+    })
+
+    // test('Count number of button in main tag', async ({ page }) => {
+    //     await page.goto(UrlScreening)
+
+    //     await expect(page.locator('main > Button')).toHaveCount(2);
+    // });
+
+    test('The Button tag name', async ({ page }) => {
+        await page.goto(UrlScreening)
+
+        await page.getByRole('button', { name: 'I found a pet' }).click();
+        
+        await page.getByRole('button', { name: 'I lost a pet' }).click();
+
+    });
+
+    test('Count number of a in main tag', async ({ page }) => {
+        await page.goto(UrlScreening)
+
+        await expect(page.locator('main > a')).toHaveCount(1);
+    });
+
+    test('A Tag', async({ page }) => {
+        await page.goto(UrlScreening)
+
+        await expect(page.locator('a')).toContainText('Skip to Homepage');
+    })
+})
