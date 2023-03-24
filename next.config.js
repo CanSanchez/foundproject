c18b95df2f0571844c2b2c92c769c5b54d8da816
CanSanchez <asanchez24@my.bcit.ca>
2023-03-24 08:24:40 -0700

    update image domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
  //create remote patterns for https://placedog.net/200/300
-    domains: ['placekitten.com', 'placedog.net']
+    domains: ['placekitten.com', 'placedog.net', 'res.cloudinary.com']
   },
 }
 
